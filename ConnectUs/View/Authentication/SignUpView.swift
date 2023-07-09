4f6482077363653642c980fe4f7156447efcd126
rohitpatil-webonise <rohit.patil@weboniselab.com>
2023-07-09 22:22:43 +0530

    Initial Setup

---
+++ ConnectUs/View/Authentication/SignUpView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpView.swift
+//  ConnectUs
+//
+//  Created by Rohit Patil on 09/07/23.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
