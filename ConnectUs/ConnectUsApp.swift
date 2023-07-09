b07f04b2ac37dd241579c58a403687e9fe1d6ffa
rohitpatil-webonise <rohit.patil@weboniselab.com>
2023-07-09 22:21:09 +0530

    Initial Commit

---
+++ ConnectUs/ConnectUsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectUsApp.swift
+//  ConnectUs
+//
+//  Created by Rohit Patil on 09/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ConnectUsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
